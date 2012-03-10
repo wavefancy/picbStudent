2eb39a786fb3be55c16d60fc955893886e6ee88d
wavefancy <wavefancy@gmail.com>
2012-03-10 22:24:12 +0800

    first initialize.

---
+++ picbStudent/src/wavefancy/Users.java
@@ -0,0 +1,90 @@
+package wavefancy;
+
+public class Users {
+	int id;
+	String fName;
+	String lName;
+	String email;
+	int iam;
+	String institution;
+	String psw;
+	String address;
+	String pcode;
+	String notes;
+	int pay;
+	int oadmin;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getIam() {
+		return iam;
+	}
+	public void setIam(int iam) {
+		this.iam = iam;
+	}
+	public String getInstitution() {
+		return institution;
+	}
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPcode() {
+		return pcode;
+	}
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public int getPay() {
+		return pay;
+	}
+	public void setPay(int pay) {
+		this.pay = pay;
+	}
+	public int getOadmin() {
+		return oadmin;
+	}
+	public void setOadmin(int oadmin) {
+		this.oadmin = oadmin;
+	}
+	
+}
